8e18a3205ab5748ed30764cf13930fae9437396b
Mark Edmondson <mark@guestfolio.com>
2015-07-28 12:00:04 -0700

    Update path to bounce api call

--- lib/sendgrid_api/methods/sub_user/bounce.rb
+++ lib/sendgrid_api/methods/sub_user/bounce.rb
@@ -2,15 +2,10 @@ module SendgridApi
   module User
     module Bounce
       def self.included(base)
-
-        def initialize(client=nil, options={})
-          @method = "user"
-          super
-        end
-
         # Get bounce list
         #
         def list_bounces(options = {})
+          client_settings
           validate_options(options, [:user])
           get("bounces", "get", options)
         end
@@ -18,8 +13,16 @@ module SendgridApi
         # Delete bounce
         #
         def delete_bounce(options = {})
+          client_settings
           validate_options(options, [:user, :email])
           post("bounces", "delete", options)
+        end
+
+        private
+
+        def client_settings
+          client.method = "user"
+          client.endpoint = "https://sendgrid.com/api"
         end
       end
     end
