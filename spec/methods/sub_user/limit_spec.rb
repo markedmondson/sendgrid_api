f4060c838f8e6f7b37c986153dce0e29f8eec049
Mark Edmondson <mark@guestfolio.com>
2016-01-19 12:20:49 -0800

    Update some "should" => "expects"

--- spec/methods/sub_user/limit_spec.rb
+++ spec/methods/sub_user/limit_spec.rb
@@ -9,7 +9,7 @@ module SendgridApi
 
     describe ".get_limit" do
       it "should return the send limit" do
-        subject.get_limit(user).body.should == [{credit: "0", credit_remain: "500", last_reset: "2013-10-31"}]
+        expect(subject.get_limit(user).body).to eq [{credit: "0", credit_remain: "500", last_reset: "2013-10-31"}]
       end
 
       it "should return an error if the user doesn't exist" do
@@ -19,13 +19,13 @@ module SendgridApi
 
     describe ".remove_limit" do
       it "should remove the send limit" do
-        subject.remove_limit(user).success?.should == true
+        expect(subject.remove_limit(user).success?).to eq true
       end
     end
 
     describe ".set_limit" do
       it "should set the send limit" do
-        subject.set_limit(user.merge(credits: 500)).success?.should == true
+        expect(subject.set_limit(user.merge(credits: 500)).success?).to eq true
       end
     end
   end
